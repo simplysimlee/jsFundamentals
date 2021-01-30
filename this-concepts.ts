dedc90296eb13f89b595158e59cfce8b745ab07c
simplysimlee <44057494+simplysimlee@users.noreply.github.com>
2021-01-30 20:19:19 +0530

    Update this-concepts.ts

--- this-concepts.ts
+++ this-concepts.ts
@@ -1,8 +1,7 @@
 //  Value of this
 
 //Every execution context also has a special variable this.It points to an object to which the current code that’s being executed belongs.
-//then this gets set to a leading parent object of a function call.
-// If there is no leading parent object, this defaults to the global object (undefined in strict mode).
+
 
 /** CASE 1 **/
 
@@ -21,6 +20,9 @@ myFunc()
 // true
 // here the there is no leading parent
 
+
+//then this gets set to a leading parent object of a function call.
+// If there is no leading parent object, this defaults to the global object (undefined in strict mode).
 /** CASE 2 --> Leading parent **/
 
 var globalThis = this
@@ -40,3 +42,18 @@ myObj.myMethod()
 // this inside: { myMethod: f }
 // false
 // true
+
+/** Case 3 - **/
+
+var myObj = {
+  myMethod: function () {    
+    console.log(this)
+  }
+}
+
+var myFunc = myObj.myMethod
+myFunc()
+
+// this --> window
+// when the function was executed at line 55 — not when it was created inside myObj, 
+ //  not when it was assigned to myFunc — but when it was invoked, it did not have a reference to a parent object. This is why this defaulted to Window.
